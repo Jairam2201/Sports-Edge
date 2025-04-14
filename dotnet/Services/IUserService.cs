df96d34b34a993a833e4dd382fa764455fb8c689
Gidituri Jayaram <140802567+Jairam2201@users.noreply.github.com>
2025-04-14 16:02:07 +0530

    Add files via upload

---
+++ dotnet/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public interface IUserService
+    {
+        User? GetUserByEmail(string email);
+        bool VerifyPassword(string enteredPassword, string dbPassword);
+        string Password(string password);
+        void CreateUser(User user);
+    }
+}
