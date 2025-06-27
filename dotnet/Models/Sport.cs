82b94bec329cead67b644b21b100d2133a85c848
Gidituri Jayaram <140802567+Jairam2201@users.noreply.github.com>
2025-06-27 23:50:20 +0530

    Delete dotnet/Models directory

--- dotnet/Models/Sport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class Sport
-    {
-        public int id { get; set; }
-        public required string name { get; set; } // Required property
-        public required string img { get; set; }  // Required property
-    }
-}
